14aa4a827caf8c1e95a6b11d806131fb2d703a87
alejomaar <demonmaar@gmail.com>
2024-09-25 22:31:30 -0500

    feat: add basic pydantic model in prediction API

--- challenge/api.py
+++ challenge/api.py
@@ -1,13 +1,26 @@
+from typing import List, Literal
+
 import fastapi
+from pydantic import BaseModel, conint
 
 app = fastapi.FastAPI()
 
+
+class FlightPrediction(BaseModel):
+    OPERA: str  
+    TIPOVUELO: Literal["N", "I"] 
+    MES: conint(ge=1, le=12)  
+
+
+class FlightPredictionRequest(BaseModel):
+    flights: List[FlightPrediction]  
+
+
 @app.get("/health", status_code=200)
 async def get_health() -> dict:
-    return {
-        "status": "OK"
-    }
+    return {"status": "OK"}
+
 
 @app.post("/predict", status_code=200)
-async def post_predict() -> dict:
-    return
+async def post_predict(request: FlightPredictionRequest) -> dict:
+    return {"predict": [0]}
