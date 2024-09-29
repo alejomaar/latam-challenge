e35e3ba202eb70acdea720d8a242e555f01ba636
alejomaar <demonmaar@gmail.com>
2024-09-28 23:03:25 -0500

    refactor: move schemas to folder

--- challenge/api.py
+++ challenge/api.py
@@ -1,38 +1,33 @@
-from enum import Enum
-from typing import List, Literal
-
 import fastapi
 import pandas as pd
-from pydantic import BaseModel, Field, conint
 
+import challenge.schema as schema
 from challenge.model import DelayModel
 
 app = fastapi.FastAPI()
 delay_model = DelayModel()
 
 
-class Flight(BaseModel):
-    OPERA: str = Field(..., description="Name of the airline that operates.")
-    TIPOVUELO: Literal["N", "I"] = Field(
-        ..., description="Type of flight, I = International, N = National."
-    )
-    MES: conint(ge=1, le=12) = Field(
-        ..., description="Number of the month of operation of the flight."
-    )
-
-
-class FlightRequest(BaseModel):
-    flights: List[Flight]
-
-
 @app.get("/health", status_code=200)
 async def get_health() -> dict:
+    """Health check endpoint.
+
+    Returns:
+        dict: A dictionary indicating the service status.
+    """
     return {"status": "OK"}
 
 
-@app.post("/predict", status_code=200)
-async def post_predict(flight_request: FlightRequest) -> dict:
+@app.post("/predict", status_code=200, response_model=list[schema.FlightResponse])
+async def post_predict(flight_request: schema.FlightRequest) -> dict:
+    """Predict delays based on flight data.
 
+    Args:
+        flight_request (FlightRequest): The flight data submitted in the request.
+
+    Returns:
+        dict: A dictionary containing the prediction results.
+    """
     data = pd.DataFrame(flight_request.dict()["flights"])
     features = delay_model.preprocess(data)
     prediction = delay_model.predict(features)
